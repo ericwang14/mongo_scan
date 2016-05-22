88d8c7739cc37b440cf789f4b3c125e052e234cb
BaCde <glacier@insight-labs.org>
2016-05-22 10:25:43 +0800

    update dockerscanner
    加强docker的漏洞检测，更加精确。

--- DockScanner.py
+++ DockScanner.py
@@ -3,21 +3,27 @@
 # author			: glacier@insight-labs.org
 # version			: v1.0
 
+import os
 import Queue
 import threading
+import requests
 import socket
+import httplib
 import time
 import urllib2
 
 ipfile ='ips.txt'
 exitFlag = 0
+threadnum = 200
 wr = open("./result.txt",'a')
 result = []
-timeout = 5
-threadnum = 200
+timeout = 5 
 socket.setdefaulttimeout(timeout)  #设置了全局默认超时时间
 urllib2.socket.setdefaulttimeout(timeout) 
 
+def my_urlencode(str) :
+       reprStr = repr(str).replace(r'\x', '%')
+       return reprStr[1:-1]
 
 class ScannerThread(threading.Thread):
     def __init__(self,threadid, q):
@@ -35,22 +41,34 @@ class RedirectHandler(urllib2.HTTPRedirectHandler):
     def http_error_302(self, req, fp, code, msg, headers):
         pass
 
-def verify_vul(url):
+def verify_vul(target):
     html = ''
+    url = target
     #url = url#+"/containers/json"
     try:
-        
         opener = urllib2.build_opener(RedirectHandler)
         response = opener.open(url,timeout=timeout)
         html = response.read()
     except urllib2.HTTPError,e:
         if(e.code==404):
-           return urlsvn  
+           return url
     except Exception, e:
     	return "error"
         pass
     return "error"
 
+def verify_vul2(url):
+    target = "http://" + url + ":2375/version"
+    try:
+        opener = urllib2.build_opener(RedirectHandler)
+        response = opener.open(target,timeout=timeout)
+        html = response.read()
+        if 'ApiVersion' in html:
+            return target
+    except Exception, e:
+        return "error"
+        pass
+    return "error"
 
 def scanner(q):
 	while not exitFlag:
@@ -62,11 +80,12 @@ def scanner(q):
 
 			if(domain !=''):
 				docker = verify_vul("http://"+domain+":2375")
-				
 				if(docker!="error"):
-					print docker + "  is exist"
-					wr.write(docker+"\n")
-					result.append(docker+"\n")
+					ret = verify_vul2(domain)
+					if(ret!="error"):
+						print ret + "  is exist"
+						wr.write(ret+"\n")
+						result.append(ret+"\n")
 	q.task_done()
 
 queueLock = threading.Lock()
